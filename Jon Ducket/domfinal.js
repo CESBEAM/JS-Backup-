e1ca770523cfae365e3b8f392e2bb3e7e5ef80bc
CESBEAM <softwareversion2.8@gmail.com>
2022-02-14 18:48:18 +0800

    grocery list

--- Jon Ducket/domfinal.js
+++ Jon Ducket/domfinal.js
@@ -1,67 +1,106 @@
-var form = document.getElementById('addForm');
-var itemList = document.getElementById('items');
-var filter = document.getElementById('filter');
+// var form = document.getElementById('addForm');
+// var itemList = document.getElementById('items');
+// var filter = document.getElementById('filter');
 
-//Form submit event
-form.addEventListener('submit', addItem)
+// //Form submit event
+// form.addEventListener('submit', addItem)
 
-//Delete event
-itemList.addEventListener('click',removeItem);
+// //Delete event
+// itemList.addEventListener('click',removeItem);
 
-//Filter event
-filter.addEventListener('keyup',filterItems)
+// //Filter event
+// filter.addEventListener('keyup',filterItems)
 
-//Add item
-function addItem(e){
+// //Add item
+// function addItem(e){
+//     e.preventDefault();
+
+//    // input input value;
+//    var newItem = document.getElementById('item').value;
+
+//    //create new li element
+//    var li = document.createElement('li');
+//    li.className = 'list-group-item';
+
+//    //Add text node with input value
+//    li.appendChild(document.createTextNode(newItem));
+
+//    //create delete button element
+//    var deleteBtn = document.createElement('button');
+//    deleteBtn.className = "btn btn-danger btn-sm float-right delete";
+
+//    deleteBtn.appendChild(document.createTextNode('x'));
+
+//    //append button to li
+//    li.appendChild(deleteBtn);
+
+//    //append li to list
+//    itemList.appendChild(li);
+// }
+
+// // Remove items
+// function removeItem(e){
+//     if(e.target.classList.contains('delete')){
+//         if(confirm('Are You Sure?')){
+//             var li = e.target.parentElement;
+//             itemList.removeChild(li); 
+//         }
+//     }
+// }
+
+// function filterItems(e){
+//     //convert text to lowercase
+//     var text = e.target.value.toLowerCase();
+//     console.log(text);
+//     //get list
+//     var items = itemList.getElementsByTagName('li');
+//     //convert to an array
+//     Array.from(items).forEach(function(item){
+//         var itemName = item.firstChild.textContent;
+//         if(itemName.toLowerCase().indexOf(text) !=-1){
+//             item.style.display = "block";
+//         }else{
+//             item.style.display = 'none';
+//         }
+//     });
+// }
+
+
+formfield = document.querySelector('#addForm');
+listItems = document.querySelector('#items');
+
+
+formfield.addEventListener('submit',addForm);
+listItems.addEventListener('click',removeItem);
+
+function addForm(e){
     e.preventDefault();
 
-   // input input value;
-   var newItem = document.getElementById('item').value;
+    var addItem = document.querySelector('#item').value;
 
-   //create new li element
-   var li = document.createElement('li');
-   li.className = 'list-group-item';
+    //create Element
+   var newLi = document.createElement('li');
+   newLi.className = "list-group-item";
+   newLi.appendChild(document.createTextNode(addItem));
 
-   //Add text node with input value
-   li.appendChild(document.createTextNode(newItem));
+   //create deleteBtn
+    var deleteBtn = document.createElement('button');
+    deleteBtn.className = "btn btn-danger btn-sm float-right delete";
+    deleteBtn.appendChild(document.createTextNode('x')); 
 
-   //create delete button element
-   var deleteBtn = document.createElement('button');
-   deleteBtn.className = "btn btn-danger btn-sm float-right delete";
+    newLi.appendChild(deleteBtn);
+    listItems.appendChild(newLi);
 
-   deleteBtn.appendChild(document.createTextNode('x'));
-
-   //append button to li
-   li.appendChild(deleteBtn);
-
-   //append li to list
-   itemList.appendChild(li);
+  
 }
 
-// Remove items
 function removeItem(e){
     if(e.target.classList.contains('delete')){
-        if(confirm('Are You Sure?')){
-            var li = e.target.parentElement;
-            itemList.removeChild(li); 
+        if(confirm('Are you sure?')){
+            var newLi = e.target.parentElement;
+            listItems.removeChild(newLi);
         }
     }
 }
 
-function filterItems(e){
-    //convert text to lowercase
-    var text = e.target.value.toLowerCase();
-    console.log(text);
-    //get list
-    var items = itemList.getElementsByTagName('li');
-    //convert to an array
-    Array.from(items).forEach(function(item){
-        var itemName = item.firstChild.textContent;
-        if(itemName.toLowerCase().indexOf(text) !=-1){
-            item.style.display = "block";
-        }else{
-            item.style.display = 'none';
-        }
-    });
-}
 
